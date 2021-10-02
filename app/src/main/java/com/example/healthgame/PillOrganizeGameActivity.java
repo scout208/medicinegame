f31edfb3e58b8906f45e2e08f624594c1638d544
Connor Early <connor-early@uiowa.edu>
2021-10-02 11:38:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/healthgame/PillOrganizeGameActivity.java
@@ -0,0 +1,13 @@
+package com.example.healthgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PillOrganizeGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
